1d01b81af64996d54fb9210a486619ea47c60ecd
Sebastian Sabbadini <sabbadini.sebastian@hotmail.de>
2019-05-18 19:59:01 +0200

    awsome, yet basic login

--- election/src/app/app.module.ts
+++ election/src/app/app.module.ts
@@ -1,17 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { AdminComponent } from './admin/admin.component';
-import { HomeComponent } from './admin/home/home.component';
-import { StationsComponent } from './admin/stations/stations.component';
-import { StationComponent } from './admin/station/station.component';
-import { ResultsComponent } from './admin/results/results.component';
-import { UserComponent } from './user/user.component';
-import { VoteComponent } from './user/vote/vote.component';
-import { ConfirmComponent } from './user/confirm/confirm.component';
-import { LoginComponent } from './login/login.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { AdminComponent } from "./admin/admin.component";
+import { HomeComponent } from "./admin/home/home.component";
+import { StationsComponent } from "./admin/stations/stations.component";
+import { StationComponent } from "./admin/station/station.component";
+import { ResultsComponent } from "./admin/results/results.component";
+import { UserComponent } from "./user/user.component";
+import { VoteComponent } from "./user/vote/vote.component";
+import { ConfirmComponent } from "./user/confirm/confirm.component";
+import { LoginComponent } from "./login/login.component";
 
 @NgModule({
   declarations: [
@@ -24,13 +25,10 @@ import { LoginComponent } from './login/login.component';
     UserComponent,
     VoteComponent,
     ConfirmComponent,
-    LoginComponent
+    LoginComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- election/src/app/login/login.component.ts
+++ election/src/app/login/login.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"],
 })
 export class LoginComponent implements OnInit {
+  users = { user1: "password1" };
 
-  constructor() { }
+  username = "";
+  password = "";
 
-  ngOnInit() {
+  constructor(private router: Router) {}
+
+  ngOnInit() {}
+
+  authenticate() {
+    if (this.users[this.username] === this.password)
+      this.router.navigate(["/user"]);
+    else alert("Good guess, but ... No!");
   }
-
 }
