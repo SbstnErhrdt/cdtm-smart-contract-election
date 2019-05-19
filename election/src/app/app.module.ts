2eb824f77b92c570169c6ef629dba245b32d6eb1
Sebastian Sabbadini <sabbadini.sebastian@hotmail.de>
2019-05-19 11:13:28 +0200

    added pie chart

--- election/src/app/app.module.ts
+++ election/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { UserComponent } from "./user/user.component";
 import { VoteComponent } from "./user/vote/vote.component";
 import { ConfirmComponent } from "./user/confirm/confirm.component";
 import { LoginComponent } from "./login/login.component";
+import { ChartsModule } from "ng2-charts";
 
 @NgModule({
   declarations: [
@@ -27,7 +28,7 @@ import { LoginComponent } from "./login/login.component";
     ConfirmComponent,
     LoginComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ChartsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
