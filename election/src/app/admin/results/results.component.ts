a9a68228f906b270328992a77fdd28df464065a1
Sebastian Erhardt <sebastian@ex-studios.net>
2019-05-19 11:57:29 +0200

    start result

--- election/src/app/admin/results/results.component.ts
+++ election/src/app/admin/results/results.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Web3Service } from 'src/util/web3.service';
 
 @Component({
   selector: 'app-results',
@@ -7,9 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResultsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private web3Service: Web3Service) {
 
-  ngOnInit() {
   }
 
+  ngOnInit() {
+    this.web3Service.artifactsToContract(election_artifacts)
+      .then((ElectionAbstraction) => {
+        this.Election = ElectionAbstraction;
+        this.Election.deployed()
+          .then(deployed => {
+            console.log(deployed);
+            deployed.pollingStationsCount.call().then((stationsCount) => {
+              const result = stationsCount.toNumber();
+              this.loadStations(result);
+            });
+          });
+      });
+  }
+
+  loadStations(candidatesCount) {
+    this.stations = [];
+
+    this.web3Service.artifactsToContract(election_artifacts)
+      .then((ElectionAbstraction) => {
+        this.Election = ElectionAbstraction;
+        this.Election.deployed()
+          .then(deployed => {
+            for (var i = 1; i <= candidatesCount; i++) {
+              deployed.getStation(i).then((station) => {
+                // append the candidates
+                this.stations.push({
+                  id: station[0].toNumber(),
+                  name: station[1],
+                  amountOfEligibleVoters: station[2].toNumber(),
+                })
+              });
+            }
+          });
+      });
+  }
 }
